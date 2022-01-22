2219930804d51893a1a0d4adb4b9add2d9ac085b
maytinhdibo <iammaytinhdibo@gmail.com>
2022-01-22 11:46:44 +0000

    sm6225-common: introduce battery-friendly-pocketmode
    DNM experimental

--- bengal.mk
+++ bengal.mk
@@ -314,6 +314,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
