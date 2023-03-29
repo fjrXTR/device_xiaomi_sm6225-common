8c665b68847e52b067f5ae6d5cbab5836881b60a
kdrag0n <danny@kdrag0n.dev>
2023-03-29 11:21:16 +0800

    sm6225-common: overlay: Compact cached app heaps in the background * Background ART heap compaction for cached apps has been recommended for Android Go devices for a while, and Google has enabled it for the Pixel 5 on RQ1A.201205.010. This feature should help fit more cached apps in memory

--- bengal.mk
+++ bengal.mk
@@ -300,6 +300,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderSM6225 \
     SettingsSM6225 \
     SystemUISM6225 \
+    SimpleDeviceConfigSM6225 \
     TelephonySM6225 \
     WifiSM6225 \
     NotchBarKiller \
