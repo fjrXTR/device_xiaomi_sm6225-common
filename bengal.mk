6e8a9caa3659e313df1d208e84370d50e2fe8f67
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-04-13 06:40:56 +0700

    sm6225-common: Update media configs from LA.UM.9.15.r1-07300-KAMORTA.QSSI13.0
    Change-Id: I3622c65858cad12dd0d81dabd06f5906addc055a

--- bengal.mk
+++ bengal.mk
@@ -269,7 +269,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/media/,$(TARGET_COPY_OUT_VENDOR)/etc) \
-    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml
+    $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
+    $(LOCAL_PATH)/media/media_profiles_vendor_khaje.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor_khaje.xml
+
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
